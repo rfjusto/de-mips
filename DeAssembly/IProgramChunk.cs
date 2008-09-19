7c54a14ee483590d16a5dfa48e3ec4ab54566baf
flyingfowlsoftware <flyingfowlsoftware@earthlink.net>
2008-09-19 16:38:46 +0000

    GUI extras, V4300 extensions, premature start on intermediate format.

---
+++ DeAssembly/IProgramChunk.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeMIPS
+{
+    interface IProgramChunk
+    {
+        string Assembly//HACK: just getting it to build
+        {
+            get;
+            set;
+        }
+
+        string Highlevel//HACK: just getting it to build
+        {
+            get;
+            set;
+        }
+    }
+}

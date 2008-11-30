243c889b599ff3d1a4cd2c6ad045d83879086d98
flyingfowlsoftware <flyingfowlsoftware@earthlink.net>
2008-11-30 19:12:58 +0000

    Michael's patch: Started loop on loop detection.

---
+++ DeAssembly/ProgramChunkLoop.cs
@@ -0,0 +1,48 @@
+﻿//This is the class used for loops (based off of ProgramChunkJumpTarget.cs)
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeMIPS
+{
+    class ProgramChunkLoop : IProgramChunk
+    {
+        #region variables
+
+        private IProgramChunk loopLabel;
+        private LinkedList<IProgramChunk> loopCode;
+
+        #endregion
+
+        #region properties
+
+        public IProgramChunk LoopLabel
+        {
+            get { return loopLabel; }
+            set { loopLabel = value; }
+        }
+
+        #endregion
+
+        #region constructor
+
+        public ProgramChunkLoop(IProgramChunk LoopLabel2)
+        {
+            LoopLabel = LoopLabel2;
+        }
+
+        #endregion
+
+
+        #region interface methods
+
+        //TODO: Implement me!
+        public bool UsesVariable(BlockVariable variable)
+        {
+            return false;
+        }
+
+        #endregion
+    }
+}
